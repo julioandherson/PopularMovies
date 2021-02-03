9810f81a7ade447555ec062ea2ddc415791e1f31
Júlio Andherson de Oliveira Silva <julio.silva@virtus.ufcg.edu.br>
2021-02-02 23:43:07 -0300

    Create tests for movie persistence

--- PopularMovies/PopularMoviesTests/PopularMoviesTests.swift
+++ PopularMovies/PopularMoviesTests/PopularMoviesTests.swift
@@ -9,18 +9,30 @@ import XCTest
 @testable import PopularMovies
 
 class PopularMoviesTests: XCTestCase {
+    let movieViewModel = MovieViewModel()
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testMoviePersistence() throws {
+        let mockMovie = Movie(id: 123456, originalTitle: "Cast Away", title: "Cas Away", overview: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.", posterPath: "posterPath/", backdropPath: "backdropPath/", voteAverage: 7.23)
+        
+        var hasMovie = movieViewModel.hasMovie(id: mockMovie.id)
+        XCTAssertFalse(hasMovie)
+        
+        movieViewModel.saveMovie(movie: mockMovie)
+        hasMovie = movieViewModel.hasMovie(id: mockMovie.id)
+        XCTAssertTrue(hasMovie)
+        
+        XCTAssert(movieViewModel.getAllMovies().count >= 1)
+
+        movieViewModel.deleteMovie(id: mockMovie.id)
+        hasMovie = movieViewModel.hasMovie(id: mockMovie.id)
+        XCTAssertFalse(hasMovie)
     }
 
     func testPerformanceExample() throws {
